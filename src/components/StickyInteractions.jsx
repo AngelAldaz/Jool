ba47be8c06ff9415584b89b984654f3b52ff2a8d
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-05-26 15:21:16 -0600

    feat(components): create StickyInteractions component
        - Implements sticky interactions bar with views, comments, and stars.
        - Uses SVG images for icons and displays counts.
        - Adds hover and click interactions for starring.

---
+++ src/components/StickyInteractions.jsx
@@ -0,0 +1,50 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function StickyInteractions({ views, comments, stars, liked }) {
+  const [isStarHovered, setIsStarHovered] = useState(false);
+  const [isLiked, setIsLiked] = useState(liked);
+  const [starCount, setStarCount] = useState(stars);
+
+  return (
+    <div className="sticky top-4 z-10 bg-white rounded-4xl shadow-card p-3 ">
+      <div className="flex gap-6 items-center justify-center">
+        <div className="flex items-center gap-2 hover:bg-gray-50 px-3 py-2 rounded-lg transition-colors">
+          <Image src="/views.svg" alt="Views" width={25} height={25} />
+          <span className="text-xs md:text-sm font-medium">{views} views</span>
+        </div>
+        <a
+          href=""
+          className="flex items-center gap-2 hover:bg-gray-50 px-3 py-2 rounded-lg transition-colors"
+        >
+          <Image src="/comment.svg" alt="Comments" width={25} height={25} />
+          <span className="text-xs md:text-sm font-medium">
+            {comments} comments
+          </span>
+        </a>
+        <a
+          href=""
+          className="flex items-center gap-2 hover:bg-gray-50 px-3 py-2 rounded-lg transition-colors"
+          onMouseEnter={() => setIsStarHovered(true)}
+          onMouseLeave={() => setIsStarHovered(false)}
+          onClick={(e) => {
+            e.preventDefault();
+            setStarCount(isLiked ? starCount - 1 : starCount + 1);
+            setIsLiked(!isLiked);
+          }}
+        >
+          <Image
+            src={isStarHovered || isLiked ? "/starFill.svg" : "/starNoFill.svg"}
+            alt="Stars"
+            width={25}
+            height={25}
+          />
+          <span className="text-xs md:text-sm font-medium">
+            {starCount} stars
+          </span>
+        </a>
+      </div>
+    </div>
+  );
+}
