4c388d96166a12326f1ad33450257a09e0734186
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-05-27 01:59:49 -0600

    feat(navbar): improve navbar responsiveness and add mobile logo
        - Adjusted navbar to be more responsive on different screen sizes.
        - Added a mobile-specific logo that displays on smaller screens.
        - Adjusted user button visibility based on screen size.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,24 @@
 import SearchBar from "./SearchBar";
 import UserButton from "./UserButton";
 import FilterButtons from "./FilterButtons";
+import Link from "next/link";
 export default function NavBar() {
   return (
-    <section className="flex flex-col gap-2  w-fit mx-auto">
-      <nav className="flex justify-center gap-3 mt-5">
+    <section className="flex flex-col gap-2 w-4/5 md:w-fit mx-auto">
+      <Link href="/">
+        <img
+          src="/JOOL.svg"
+          alt="Jool Logo"
+          className="block md:hidden h-20 mx-auto "
+        />
+      </Link>
+      <div className="md:hidden block">
         <UserButton />
+      </div>
+      <nav className="flex justify-center gap-3 md:mt-5">
+        <div className="md:block hidden">
+          <UserButton />
+        </div>
         <SearchBar />
       </nav>
       <div className="flex justify-end">
